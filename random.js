2c64b6ad9400cfba7bebff14fe6dec185fa2449f
Instructor <dodydev311@gmail.com>
2024-07-10 18:48:08 +0530

    dice

---
+++ random.js
@@ -0,0 +1,6 @@
+
+
+let arr = ["apple", "banana", "cherry", "date", "elderberry"];
+
+let randomIndex = parseInt(Math.random()*5) // 0 - 4
+console.log(randomIndex, arr[randomIndex]) // 0 - 4.9999999
